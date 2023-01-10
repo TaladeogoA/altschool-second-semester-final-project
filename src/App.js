b9aef100b0d33e8e1e0a57d2bd2dee5e0182f690
Taladeogo Abraham <ataladeogo@gmail.com>
2023-01-10 19:17:37 +0100

    Added function to sort repos by last updated

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import ErrorPage from "./components/errorPage/ErrorPage";
 
 // Implement an API fetch of your GitHub portfolio, show a page with a list of all your repositories on GitHub(the page should implement pagination for the repo list), and create another page showing data for a single repo clicked from the list of repos using nested routes while using all the necessary tools in react. Implement the proper SEO, Error Boundary (show a page to test the error boundary) and 404 pages. Good UI and Designs are important.
 
-// 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
-
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
